b3eef5f7d4d1800e2e4e4281d01c122cc086192f
WillyFFS <willyfrans13@gmail.com>
2023-12-08 15:36:50 +0700

    syntaxnupload gambar

--- view/src/app/tes/page.js
+++ view/src/app/tes/page.js
@@ -4,11 +4,11 @@ import { useState } from 'react'
 
 function App() {
   
-  const [postImage, setPostImage] = useState( {
+  const [newKamar, setNewKamar] = useState( {
     noKamar:"99a", 
     foto:"", 
     harga: 100000, 
-    lantai:"", 
+    lantai:"5", 
     status: true, 
     fasilitas:"aa", 
     luas:"100x400", 
@@ -16,16 +16,46 @@ function App() {
     idUser: "sfsdfs", 
     namaUser: "sdfsdf"});
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Uploaded")
+  const handleSubmit = async () => {
+    const noKamar= newKamar.noKamar
+    const foto = newKamar.foto
+    const harga= newKamar.harga
+    const lantai = newKamar.lantai
+    const status= newKamar.status
+    const fasilitas= newKamar.fasilitas
+    const luas= newKamar.luas
+    const posisi= newKamar.posisi
+    const idUser= newKamar.idUser
+    const namaUser= newKamar.namaUser
+    try{
+        console.log("Uploaded");
+        const response = await fetch('http://localhost:3000/api/kamar',{
+            method: "POST",
+            body: JSON.stringify({
+                noKamar,
+                foto,
+                harga,
+                lantai,
+                status,
+                fasilitas,
+                luas,
+                posisi,
+                idUser,
+                namaUser
+            }),
+            headers: { "Content-Type": "application/json" },
+        })
+    }catch(error){
+        console.log(error);
+    }
+    
   }
 
   const handleFileUpload = async (e) => {
     const file = e.target.files[0];
     const base64 = await convertToBase64(file);
     console.log(base64)
-    setPostImage({ ...postImage, foto : base64 })
+    setNewKamar({ ...newKamar, foto : base64 })
   }
 
   return (
@@ -33,12 +63,12 @@ function App() {
       <form onSubmit={handleSubmit}>
 
         <label htmlFor="file-upload" className='custom-file-upload'>
-          <img src={postImage.myFile } alt="" />
+          <img src={newKamar.foto } alt="" />
         </label>
 
         <input 
           type="file"
-          name="myFile"
+          name="foto"
           accept='.jpeg, .png, .jpg'
           onChange={(e) => handleFileUpload(e)}
          />
