7ea987566952e99b872493b3c893ce4ecd966f1f
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-12-08 12:36:59 +0700

    feat: add backend read all kamar

--- view/src/components/ListKamar.jsx
+++ view/src/components/ListKamar.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import room from '../../public/room.jpg'
 
-function ListKamar() {
+function ListKamar(props) {
+  const {noKamar, harga} = props;
+
+  //Algoritma pengubah rupiah
+  const numberFormat = (value) =>
+  new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(value);
+
   return (
     <a href="/rooms/detil-kamar" className='max-w-[300px] mx-auto h-[292px]'>
         <img src={room.src} alt="Foto Kamar" className='w-full h-[225px]' />
         <div className="w-full h-[67px] p-2.5 flex flex-col justify-center items-start gap-[3px]">
             <div className="self-stretch h-[47px] flex flex-col justify-center items-center gap-[3px]">
-                <div className="self-stretch text-base font-medium font-['Inter']">Kamar 5A</div>
+                <div className="self-stretch text-base font-medium font-['Inter']">Kamar {noKamar}</div>
                 <div className="self-stretch h-[25px] flex justify-between items-start">
-                <div className="grow shrink basis-0 text-red-600 text-xl font-semibold font-['Inter']">Rp1.xxx.xxx/Month</div>
+                <div className="grow shrink basis-0 text-red-600 text-xl font-semibold font-['Inter']">{numberFormat(harga)}/Month</div>
                 </div>
             </div>
         </div>
